d2cbc6039022137383a46fd1e3d1c1d12648fd74
Harshaa Bajaj <harshanbajaj@gmail.com>
2018-07-10 12:41:50 -0500

    Update div_array.py

--- div_array.py
+++ div_array.py
@@ -2,9 +2,8 @@ def div_arr(a):
     '''
         Description : split array into two such that sum of elements in both is equal
         Input : array
-        Output : list of two sub arrays
+        Output : splitable index
     '''
-    index = [] # to save the split index
     total = sum(a) - a[0] # whole sum - first element to begin with
     seen = a[0] # already seen element total
     for i in range(len(a)-1): # repeat for all elements 
@@ -13,17 +12,19 @@ def div_arr(a):
             total -= a[i+1] # subtract this element from total
             seen += a[i+1] # add this element to seen total
         else : # if they are equal we can split
-            index.append(i+1) # split index
-            break # no need to search further
-    if len(index)>0: # if we found a split index
-        return([a[0:index[0]],a[index[0]:]]) # return list of the two new arrays
-    else: return(a) # if not found return the original array
+            return i + 1
 
-# example call:
-# a=[1,2,3,4,5,5] ; b=[5,5,1,2,3,4]
-# print(div_arr(a)) ; print(div_arr(b)) or list(map(div_arr,[a,b]))
-
+if __name__ == '__main__':
+	a = input('Please enter the list \n') or '2,7,9'
+    a = [int(x) for x in a.split()]
+    res = div_arr(a)
+    if res != None: # if we found a split index
+        print('Array splitable \n',[a[0:res],a[res:]]) # return list of the two new arrays
+    else: print('Unable to split\n',a) # if not found return the original array
+    
+# example input:
+# a=[1,2,3,4,5,5] ; b=[5,5,1,2,3,4]; c = [3,4]
 # expected output :
-# [[1, 2, 3, 4], [5, 5]]
-# [[5, 5], [1, 2, 3, 4]] or
-# [[[1, 2, 3, 4], [5, 5]], [[5, 5], [1, 2, 3, 4]]]
+# Array splitable : [[1, 2, 3, 4], [5, 5]]
+# Array splitable : [[5, 5], [1, 2, 3, 4]] 
+# Unable to split : [3,4]
